6307ded00b92d9013c375b438fcbb007687005fc
Zachary Jager LaPointe <zachary.lapointe@mail.utoronto.ca>
2022-02-04 04:07:51 -0500

    Adding iterative weighted A* and iterative greedy best-first search implementations. Going to work on improving heuristic now

--- A1/solution.py
+++ A1/solution.py
@@ -28,11 +28,24 @@ def heur_alternate(state):
 
     # very simple, will improve if I have time, mostly focused on 
     # "punishing" bad states, i.e. unsolvable ones right now
+
+    # we also add on the number of steps for the closest robot to move to the closest box
+
     return \
         sum([
             evaluate_box(box, state)
             for box
             in state.boxes
+        ]) + \
+        min([
+            min([
+                mh_dist(robot, box)
+                for box
+                in state.boxes
+                #if box not in state.storage
+            ])
+            for robot
+            in state.robots
         ])
 
 
@@ -68,7 +81,6 @@ def evaluate_box(box, state):
         in state.storage
     ])
 
-
 def heur_zero(state):
     '''Zero Heuristic can be used to make A* search perform uniform cost search'''
     return 0
@@ -79,6 +91,8 @@ def heur_manhattan_distance(state):
     '''INPUT: a sokoban state'''
     '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''
 
+    # calculates the manhattan distance between each box and store, and finds the minimum from each box,
+    # and then sums them all up for the total heuristic
     total = \
         sum([
             min([
@@ -99,7 +113,6 @@ def mh_dist(box, store):
 
 
 def fval_function(sN, weight):
-    # IMPLEMENT
     """
     Provide a custom formula for f-value computation for Anytime Weighted A star.
     Returns the fval of the state contained in the sNode.
@@ -114,13 +127,12 @@ def fval_function(sN, weight):
 
 
 def weighted_astar(initial_state, heur_fn, weight, timebound):
-    # IMPLEMENT
     '''Provides an implementation of weighted a-star, as described in the HW1 handout'''
     '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
     '''OUTPUT: A goal state (if a goal is found), else False as well as a SearchStats object'''
     '''implementation of weighted astar algorithm'''
 
-    se = SearchEngine('custom')
+    se = SearchEngine('custom', 'full')
 
     wrapped_fval_function = (lambda sN: fval_function(sN, weight))
     se.init_search(initial_state,sokoban_goal_state,heur_fn,wrapped_fval_function)
@@ -130,18 +142,54 @@ def weighted_astar(initial_state, heur_fn, weight, timebound):
 
 
 # uses f(n), see how autograder initializes a search line 88
-def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):
+def iterative_astar(initial_state, heur_fn, weight=10, timebound=5):
     '''Provides an implementation of realtime a-star, as described in the HW1 handout'''
     '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
     '''OUTPUT: A goal state (if a goal is found), else False as well as a SearchStats object'''
     '''implementation of iterative astar algorithm'''
-    return None, None  # CHANGE THIS
+
+    se = SearchEngine('custom')
+    current_weight = weight
+    end = os.times()[0] + timebound
+    result = False
+    solution = False
+    cost_limits = None
+
+    se.init_search(initial_state, sokoban_goal_state, heur_fn,
+                   (lambda sN: fval_function(sN, current_weight)))
+    solution, solution_stats = se.search(end-os.times()[0])
+
+    while result is not False:
+        current_weight = current_weight**0.5
+        se.init_search(initial_state, sokoban_goal_state, heur_fn,
+                       (lambda sN: fval_function(sN, current_weight)))
+        result, stats = se.search(end-os.times()[0], cost_limits)
+        if result:
+            solution = result
+            solution_stats = stats
+            cost_limits = (result.gval, float('inf'), result.gval)
+    return solution, solution_stats
 
 
 def iterative_gbfs(initial_state, heur_fn, timebound=5):  # only use h(n)
-    # IMPLEMENT
     '''Provides an implementation of anytime greedy best-first search, as described in the HW1 handout'''
     '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
     '''OUTPUT: A goal state (if a goal is found), else False'''
     '''implementation of iterative gbfs algorithm'''
-    return None, None  # CHANGE THIS
+    
+    se = SearchEngine('best_first')
+    end = os.times()[0] + timebound
+    result = False
+    solution = False
+    cost_limits = None
+
+    se.init_search(initial_state, sokoban_goal_state, heur_fn)
+    solution, solution_stats = se.search(end-os.times()[0])
+
+    while result is not False:
+        result, stats = se.search(end-os.times()[0], cost_limits)
+        if result:
+            solution = result
+            solution_stats = stats
+            cost_limits = (result.gval, float('inf'),float('inf'))
+    return solution, solution_stats
